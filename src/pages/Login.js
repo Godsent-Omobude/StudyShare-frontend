f1d0672a6baedc7302b7cdc9282b8428700a6260
Godsent-Omobude <godsentomobude@gmail.com>
2026-07-26 09:18:47 +0100

    Use environment variable for backend API URL

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,7 +18,7 @@ export default function Login() {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { username, password });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { username, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('fullName', res.data.fullName);
       navigate('/');
