337c062f633064b3714a3d611b9144e2099378f7
Godsent-Omobude <godsentomobude@gmail.com>
2026-07-28 02:13:41 +0100

    Update frontend API URL configuration

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -19,7 +19,7 @@ export default function Register() {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', { fullName, username, password });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/register`, { fullName, username, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('fullName', res.data.fullName);
       navigate('/');
