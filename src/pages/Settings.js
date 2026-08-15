42b7a1db1afb292671f0f4e5ce3982150c893782
Godsent-Omobude <godsentomobude@gmail.com>
2026-08-15 10:31:40 +0100

    Remove ESLite

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -85,11 +85,6 @@ export default function Settings() {
     };
 
     load();
-
-    return () => {
-      if (profilePictureUrl) URL.revokeObjectURL(profilePictureUrl);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const saveProfile = async (event) => {
